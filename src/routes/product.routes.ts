153c674e4761a1235508da76167ee97b06692d13
Angel Chavez <agchavez@unah.hn>
2021-09-13 22:36:04 -0600

    produtcs

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,1 +1,12 @@
-const testasd:string = "asdad";
+import { Router,  } from "express";
+import  { check } from 'express-validator';
+
+import validatorCamp from "../middlewares/validatorCamp";
+import { getAllUser, getUserByID, registerUser } from '../controllers/user.controller';
+import { emailExist } from '../helpers/dbValidator';
+import { validatorJwt } from "../helpers/jwt.helper";
+
+
+const route = Router();
+
+route.get();
